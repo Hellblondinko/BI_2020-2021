93805d766f154c2d485f55177956b60bf5127f51
Alexandra Tsitrina <sashulka.s@gmail.com>
2020-09-06 22:23:07 +0300

    Calculator which takes first number, operation and second number

---
+++ calculator.py
@@ -0,0 +1,21 @@
+a = float(input())
+c = input()1-=
+b = float(input())
+
+if c == '+':
+    print (a + b)
+elif c == '-':
+    print (a - b)
+elif c == '*':
+    print (a * b)
+elif c == 'pow':
+    print(a ** b)
+elif b == 0:
+    print ('Деление на 0!')
+elif c ==  'mod' and b!=0:
+    print(a % b)
+
+elif c == 'div' and b!=0:
+    print(a // b)
+elif c == '/' and b!=0:
+    print(a / b)
