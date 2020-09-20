638909dc77b8b8fc3bda325f9b4e5cf31ec11cc6
Alexandra Tsitrina <70773915+Hellblondinko@users.noreply.github.com>
2020-09-20 16:53:47 +0300

    Add files via upload
    Morze to English translation

---
+++ Morze_translator.py
@@ -0,0 +1,32 @@
+
+
+mor = {'.-': 'A',   '-...': 'B',   '-.-.': 'C',
+       '-..': 'D',      '.': 'E',   '..-.': 'F',
+        ' -.': 'G',   '....': 'H',     '..': 'I',
+      '.---': 'J',    '-.-': 'K',   '.-..': 'L',
+        '--': 'M',     '-.': 'N',    '---': 'O',
+      '.--.': 'P',   '--.-': 'Q',    '.-.': 'R',
+       '...': 'S',      '-': 'T',    '..-': 'U',
+      '...-': 'V',    '.--': 'W',   '-..-': 'X',
+      '-.--': 'Y',   '--..': 'Z',  '-----': '0',
+     '.----': '1',  '..---': '2',  '...--': '3',
+     '....-': '4',  '.....': '5',  '-....': '6',
+     '--...': '7',  '---..': '8',  '----.': '9'}
+print('''Enter your msg in Morse.
+Notic that:
+    1- Use / to separate the letters and space to separate words.
+    2- Your message must contain only letters and numbers.
+    3- '?' in output means that your input was unknowed.
+    >>> ''', end = '')
+msg = input(' ')
+out = []
+letter = []
+j = -1
+for i in msg.split(' '):
+    j += 1
+    letter += [i.split('/')]
+    for k in range(len(letter[j])):
+        out += mor.get(letter[j][k], '?')
+    out += ' '
+print('\n      >>> Your msg is: ', end = '')
+print(''.join(out))
